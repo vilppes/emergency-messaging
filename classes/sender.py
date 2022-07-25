9a40642726f9b2800d5a4d7ba4e762e58da2bc39
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-25 15:44:55 +0300

    used the unused imports in __init__

--- classes/sender.py
+++ classes/sender.py
@@ -3,3 +3,9 @@ from ememail import EmergencyMessageByEmail
 from emsms import EmergencyMessageByTextMessage
 from emtw import EmergencyMessageByTwitter
 from emdc import EmergencyMessageByDiscord
+class EmergencyMessageSender:
+    def __init__(self,message) -> None:
+        self.message_by_email = EmergencyMessageByEmail("name@domain.tld")
+        self.message_by_text = EmergencyMessageByTextMessage("911")
+        self.message_by_tweet = EmergencyMessageByTwitter(message)
+        self.message_by_discord = EmergencyMessageByDiscord(message)
